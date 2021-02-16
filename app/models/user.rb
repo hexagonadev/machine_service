680c2814573c4909fafaba36b76c8e824cfb693b
Carlos Torrealba <calrrox@gmail.com>
2021-02-15 20:03:42 -0400

    Add user

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ActiveRecord::Base
+  # has_many :vehicles
+  # has_many :maintenance_appointments
+  # has_many :social_networks
+  # has_many :appointments
+  # has_and_belongs_to_many :roles
+  # has_one :profile
+
+  validates :email, presence: true
+  validates :email, uniqueness: true
+
+end
