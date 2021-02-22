dc7af02e288690468aed720d188563aefaf4aab2
Carlos Torrealba <calrrox@gmail.com>
2021-02-22 17:11:30 -0400

    Add create use

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,37 @@
 class UsersController < ApplicationController
+  skip_before_action :verify_authenticity_token
 
   def index
     users = User.all
 
     respond_to do |format|
-      format.html { render inline: "<h1> Hello Rails </h1>" }
+      format.html { render inline: "<h1> Hello Users#Index </h1>" }
       format.json { render json: users }
+    end
+  end
+
+  def show
+    user = User.find_by(id: params[:id])
+
+    respond_to do |format|
+      format.html { render inline: "<h1> Hello Users#Show </h1>" }
+      format.json { render json: user }
+    end
+  end
+
+  def create
+    user = User.new(name: params[:name], last_name: params[:last_name], email: params[:email])
+
+    if user.save
+      respond_to do |format|
+        format.html { render inline: "<h1> Hello Users#Create </h1>" }
+        format.json { render json: user }
+      end
+    else
+      respond_to do |format|
+        format.html { render inline: "<h1> Hello Users#Create </h1>" }
+        format.json { render json: user.errors.full_messages }
+      end
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,16 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
  get '/users', controller: :users, action: :index
+ get '/users/:id/', controller: :users, action: :show
+
+ post '/users', controller: :users, action: :create
 #  post
 #  put
 #  patch
 #  delete
 end
 
+# http://localhost:3000/users/1
 
 # get [:index, :show]
 # post [:create]
